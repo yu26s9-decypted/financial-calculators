16958c0e1449f1e1057d2bdd12f62a3dcb30dffb
andy <andyt.cyb@gmail.com>
2026-04-13 13:56:13 -0400

    (feat) Added AnnuityCalculator

---
+++ src/main/java/AnnuityCalculator.java
@@ -0,0 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AnnuityCalculator {
+    public static double calculatePresentAnnuity(double monthlyPayout, double expectedInterest, int payoutYearLength){
+        double r = expectedInterest / 100 / 12;
+        double n = payoutYearLength * 12;
+        return monthlyPayout * (1 - Math.pow(1 + r, -n)) / r;
+
+    }
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    // Defensive Programming
+//
+
+    public static double askForDouble(String prompt, Scanner scanner){
+        System.out.printf(prompt);
+        double value = 0;
+        boolean isValidInput = false;
+
+        while (!isValidInput){
+            try {
+                value = scanner.nextDouble();
+                scanner.nextLine();
+                isValidInput = true;
+
+//                Input Mismtch: checks if the user input data matches the expected datatype.
+            } catch (InputMismatchException exception) {
+                scanner.nextLine();
+                System.out.printf("There is a input mismatch, please type your input again: %s", prompt);
+            }
+        }
+        return value;
+    }
+
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        double monthlyPayout = askForDouble("What is your desired monthly payout? $", scanner);
+        double expectedInterest = askForDouble("What is your expected interest? (in percentages): ", scanner);
+        double payoutTimeInYears = askForDouble("How many years until payout? (yrs): ", scanner);
+        double presentAnnuityValue  = calculatePresentAnnuity(monthlyPayout, expectedInterest, (int)payoutTimeInYears);
+
+        System.out.printf("To fund a annuity that pays $%,.2f for %.0f years and earn an expected %.2f%% interest, you would need to invest $%,.2f today.", monthlyPayout, payoutTimeInYears, expectedInterest, presentAnnuityValue);
+    }
+}
